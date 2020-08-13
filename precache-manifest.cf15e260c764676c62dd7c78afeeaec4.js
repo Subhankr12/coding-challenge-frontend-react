3420cb498c84d8b7f1715596fa2259cc22698b28
Subhankr12 <subhankr12@gmail.com>
2020-08-13 23:46:57 +0530

    Updates

---
+++ precache-manifest.cf15e260c764676c62dd7c78afeeaec4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343f0c65a46dd7c02507c564f22b208a",
+    "url": "/coding-challenge-frontend-react/index.html"
+  },
+  {
+    "revision": "fdeccad81102eaad8b08",
+    "url": "/coding-challenge-frontend-react/static/css/2.36593667.chunk.css"
+  },
+  {
+    "revision": "d94120689cf48b44bf74",
+    "url": "/coding-challenge-frontend-react/static/css/main.d91005fb.chunk.css"
+  },
+  {
+    "revision": "fdeccad81102eaad8b08",
+    "url": "/coding-challenge-frontend-react/static/js/2.58e6a66c.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/coding-challenge-frontend-react/static/js/2.58e6a66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d94120689cf48b44bf74",
+    "url": "/coding-challenge-frontend-react/static/js/main.8a196609.chunk.js"
+  },
+  {
+    "revision": "c19cd9a2d18c4b28a889",
+    "url": "/coding-challenge-frontend-react/static/js/runtime-main.75466584.js"
+  },
+  {
+    "revision": "ecfb8dfe6c531d9600d0e93266473178",
+    "url": "/coding-challenge-frontend-react/static/media/bike.ecfb8dfe.png"
+  },
+  {
+    "revision": "f16c75345da3e1e00c8fd1693280e886",
+    "url": "/coding-challenge-frontend-react/static/media/loader.f16c7534.gif"
+  },
+  {
+    "revision": "73eafb763acbe7ec6a21c9d321f4bf92",
+    "url": "/coding-challenge-frontend-react/static/media/logo.73eafb76.png"
+  }
+]);
